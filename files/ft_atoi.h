5fb02817ff0b84ca9ef5fe5849feccab51b5f6a6
alain <adupav2000@gmail.com>
2019-11-01 00:01:16 +0100

    finished the untested ft_atoi

--- files/ft_atoi.h
+++ files/ft_atoi.h
@@ -2,5 +2,26 @@
 
 int ft_atoi(const char *str)
 {
-    
+    long int safety_ret_val;
+    int i;
+    int pos;
+
+    pos = 1;
+    i = 0;
+    safety_ret_val = 0;
+    while(str[i] == ' ')
+        i++;    
+    while(str[i] == ('-' | '+'))
+        if(str[i++] == '-')
+            pos *= -1;
+    while(str[i] <= '9' && str[i] >= '0')
+    {
+        if(safety_ret_val * 10 * pos + ((long int)str[i] - 48) > 2147483647)
+            return safety_ret_val;
+        if(safety_ret_val * 10 * pos + ((long int)str[i] - 48) < -2147483648)
+            return safety_ret_val;
+        safety_ret_val = safety_ret_val * 10 + ((long int)str[i++] - 48);
+    }
+    return  ((int)safety_ret_val * pos);
 }
+
